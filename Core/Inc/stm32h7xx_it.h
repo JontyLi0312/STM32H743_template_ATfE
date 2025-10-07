eb68cda51115718f352eeacc737f316afb550381
JontyLi <ljt20030312@Outlook.com>
2025-10-07 11:43:17 +0800

    fix(core): 移除QUADSPI中断处理函数并调整GPIO速度设置
    QUADSPI_IRQHandler函数已被移除，不再需要处理QUADSPI中断。
    PB2, PB6, PD11, PD12, PD13, PE2的GPIO速度设置已从GPIO_SPEED_FREQ_HIGH调整为GPIO_SPEED_FREQ_VERY_HIGH，以支持更高的数据传输速率。

--- Core/Inc/stm32h7xx_it.h
+++ Core/Inc/stm32h7xx_it.h
@@ -58,7 +58,6 @@ void SysTick_Handler(void);
 void DMA1_Stream0_IRQHandler(void);
 void DMA1_Stream1_IRQHandler(void);
 void USART1_IRQHandler(void);
-void QUADSPI_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
