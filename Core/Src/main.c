bcca6c7f90c76198f8aefe8b795d6f5e5dc504ea
JontyLi <ljt20030312@Outlook.com>
2025-10-28 20:18:06 +0800

    test(Core/Src/main.c): 移除SDRAM测试函数

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -64,7 +64,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-PLACE_IN_QSPI_SECTION const uint8_t qspi[]    = "\r\nQSPI";
+PLACE_IN_QSPI_SECTION const uint8_t qspi[]    = "\r\ntest";
 PLACE_IN_QSPI_SECTION const uint8_t success[] = "\r\nOK";
 PLACE_IN_QSPI_SECTION const uint8_t fail[]    = "\r\nFAIL";
 /* USER CODE END PV */
@@ -73,95 +73,7 @@ PLACE_IN_QSPI_SECTION const uint8_t fail[]    = "\r\nFAIL";
 void SystemClock_Config(void);
 static void MPU_Config(void);
 /* USER CODE BEGIN PFP */
-uint8_t SDRAM_Test(void)
-{
-    uint32_t i        = 0; // 计数变量
-    uint16_t ReadData = 0; // 读取到的数据
-    uint8_t ReadData_8b;
 
-    uint32_t ExecutionTime_Begin; // 开始时间
-    uint32_t ExecutionTime_End;   // 结束时间
-    uint32_t ExecutionTime;       // 执行时间
-    float ExecutionSpeed;         // 执行速度
-
-    // printf("\r\n***************************************************************"
-    //        "**************************************\r\n");
-    // printf("\r\n进行速度测试>>>\r\n");
-
-    // 写入
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    ExecutionTime_Begin = HAL_GetTick(); // 获取 systick 当前时间，单位ms
-
-    for (i = 0; i < SDRAM_Size / 2; i++) {
-        *(__IO uint16_t *)(SDRAM_BANK_ADDR + 2 * i) = (uint16_t)i; // 写入数据
-    }
-    ExecutionTime_End = HAL_GetTick(); // 获取 systick 当前时间，单位ms
-    ExecutionTime =
-        ExecutionTime_End - ExecutionTime_Begin; // 计算擦除时间，单位ms
-    ExecutionSpeed = (float)SDRAM_Size / 1024 / 1024 / ExecutionTime *
-                     1000; // 计算速度，单位 MB/S
-
-    // printf("\r\n以16位数据宽度写入数据，大小：%d MB，耗时: %d ms, "
-    //        "写入速度：%.2f MB/s\r\n",
-    //        SDRAM_Size / 1024 / 1024, ExecutionTime, ExecutionSpeed);
-
-    // 读取
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    ExecutionTime_Begin = HAL_GetTick(); // 获取 systick 当前时间，单位ms
-
-    for (i = 0; i < SDRAM_Size / 2; i++) {
-        ReadData =
-            *(__IO uint16_t *)(SDRAM_BANK_ADDR + 2 * i); // 从SDRAM读出数据
-    }
-    ExecutionTime_End = HAL_GetTick(); // 获取 systick 当前时间，单位ms
-    ExecutionTime =
-        ExecutionTime_End - ExecutionTime_Begin; // 计算擦除时间，单位ms
-    ExecutionSpeed = (float)SDRAM_Size / 1024 / 1024 / ExecutionTime *
-                     1000; // 计算速度，单位 MB/S
-
-    // printf(
-    //     "\r\n读取数据完毕，大小：%d MB，耗时: %d ms, 读取速度：%.2f
-    //     MB/s\r\n", SDRAM_Size / 1024 / 1024, ExecutionTime, ExecutionSpeed);
-
-    // 数据校验
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    // printf("\r\n***************************************************************"
-    //        "**************************************\r\n");
-    // printf("\r\n进行数据校验>>>\r\n");
-
-    for (i = 0; i < SDRAM_Size / 2; i++) {
-        ReadData =
-            *(__IO uint16_t *)(SDRAM_BANK_ADDR + 2 * i); // 从SDRAM读出数据
-        if (ReadData !=
-            (uint16_t)i) // 检测数据，若不相等，跳出函数,返回检测失败结果。
-        {
-            // printf("\r\nSDRAM测试失败！！\r\n");
-            return ERROR; // 返回失败标志
-        }
-    }
-
-    // printf("\r\n16位数据宽度读写通过，以8位数据宽度写入数据\r\n");
-    for (i = 0; i < 255; i++) {
-        *(__IO uint8_t *)(SDRAM_BANK_ADDR + i) = (uint8_t)i;
-    }
-    // printf("写入完毕，读取数据并比较...\r\n");
-    for (i = 0; i < 255; i++) {
-        ReadData_8b = *(__IO uint8_t *)(SDRAM_BANK_ADDR + i);
-        if (ReadData_8b !=
-            (uint8_t)i) // 检测数据，若不相等，跳出函数,返回检测失败结果。
-        {
-            // printf("8位数据宽度读写测试失败！！\r\n");
-            // printf("请检查NBL0和NBL1的连接\r\n");
-            return ERROR; // 返回失败标志
-        }
-    }
-    // printf("8位数据宽度读写通过\r\n");
-    // printf("SDRAM读写测试通过，系统正常\r\n");
-    return SUCCESS; // 返回成功标志
-}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -228,13 +140,6 @@ int main(void)
     if (QSPI_W25Qxx_Init() != QSPI_W25Qxx_OK) { Error_Handler(); }
     if (QSPI_W25Qxx_Reset() != QSPI_W25Qxx_OK) { Error_Handler(); }
     if (QSPI_W25Qxx_MemoryMappedMode() != QSPI_W25Qxx_OK) { Error_Handler(); }
-    if (SDRAM_Test() == SUCCESS) {
-        SCB_CleanDCache_by_Addr((uint32_t *)success, sizeof(success));
-        HAL_UART_Transmit(&huart1, success, sizeof(success), HAL_MAX_DELAY);
-    } else {
-        SCB_CleanDCache_by_Addr((uint32_t *)fail, sizeof(fail));
-        HAL_UART_Transmit(&huart1, fail, sizeof(fail), HAL_MAX_DELAY);
-    }
     // lv_init();
     // lv_port_disp_init();
     // lv_port_indev_init();
