c3a278007bc0658fa94ce06a82bc213815e52254
JontyLi <ljt20030312@Outlook.com>
2025-12-12 18:43:31 +0800

    feat(App\Inc\map.h): define buffer

--- App/Inc/map.h
+++ App/Inc/map.h
@@ -1,8 +1,15 @@
 #ifndef MAP_H_
 #define MAP_H_
 
-#define QSPI_FLASH ".qspi_assets_section"
+#define BUFFER                  ".buffer"
+#define PLACE_IN_BUFFER_SECTION __attribute__((section(BUFFER)))
+
+#define QSPI_FLASH              ".qspi_section"
 #define PLACE_IN_QSPI_SECTION                                                  \
     __attribute__((section(QSPI_FLASH))) __attribute__((used))
+
+#define SDRAM ".lvgl_buffers"
+#define PLACE_IN_SDRAM_SECTION                                                 \
+    __attribute__((section(SDRAM))) __attribute__((aligned(32)))
 
 #endif
