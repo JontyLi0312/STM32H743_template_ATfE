969eb320b0fec0e90eb933e6c18e745468037a5c
JontyLi <ljt20030312@Outlook.com>
2025-12-13 23:35:47 +0800

    🌈 style(App\Inc\map.h): remove redundancy define

--- App/Inc/map.h
+++ App/Inc/map.h
@@ -1,15 +1,12 @@
 #ifndef MAP_H_
 #define MAP_H_
 
-#define BUFFER                  ".buffer"
-#define PLACE_IN_BUFFER_SECTION __attribute__((section(BUFFER)))
+#define PLACE_IN_BUFFER_SECTION __attribute__((section(".buffer")))
 
-#define QSPI_FLASH              ".qspi_section"
 #define PLACE_IN_QSPI_SECTION                                                  \
-    __attribute__((section(QSPI_FLASH))) __attribute__((used))
+    __attribute__((section(".qspi_section"))) __attribute__((used))
 
-#define SDRAM ".lvgl_buffers"
 #define PLACE_IN_SDRAM_SECTION                                                 \
-    __attribute__((section(SDRAM))) __attribute__((aligned(32)))
+    __attribute__((section("sdram_buffer"))) __attribute__((aligned(32)))
 
 #endif
