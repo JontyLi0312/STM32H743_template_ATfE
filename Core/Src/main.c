4331d384569d92c5f6eb63dc16b82befbeba0fd0
JontyLi <ljt20030312@Outlook.com>
2025-12-13 18:47:35 +0800

    🦄 refactor(Core\Src\main.c): remove lvgl

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -31,10 +31,6 @@
 /* USER CODE BEGIN Includes */
 #include "lcd_show.h"
 #include "lcd_test.h"
-#include "lv_demo_music.h"
-#include "lv_port_disp.h"
-#include "lv_port_indev.h"
-#include "lvgl.h"
 #include "map.h"
 #include "qspi_w25q64.h"
 #include "sdram.h"
@@ -132,11 +128,6 @@ int main(void)
     SDRAM_Initialization_Sequence(&hsdram1);
     LCD_RGB_Init();
     Touch_Init();
-    lv_init();
-    lv_port_disp_init();
-    lv_port_indev_init();
-
-    lv_demo_music();
 
     /* USER CODE END 2 */
 
@@ -145,7 +136,6 @@ int main(void)
     while (1) {
         SCB_CleanDCache_by_Addr((uint32_t *)qspi, sizeof(qspi));
         HAL_UART_Transmit_DMA(&huart1, qspi, sizeof(qspi));
-        // lv_task_handler();
         Touch_Scan();
         HAL_Delay(1000);
         /* USER CODE END WHILE */
@@ -177,25 +167,27 @@ void SystemClock_Config(void)
     /** Initializes the RCC Oscillators according to the specified parameters
      * in the RCC_OscInitTypeDef structure.
      */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-    RCC_OscInitStruct.LSIState       = RCC_LSI_ON;
-    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM       = 5;
-    RCC_OscInitStruct.PLL.PLLN       = 192;
-    RCC_OscInitStruct.PLL.PLLP       = 2;
-    RCC_OscInitStruct.PLL.PLLQ       = 4;
-    RCC_OscInitStruct.PLL.PLLR       = 2;
-    RCC_OscInitStruct.PLL.PLLRGE     = RCC_PLL1VCIRANGE_2;
-    RCC_OscInitStruct.PLL.PLLVCOSEL  = RCC_PLL1VCOWIDE;
-    RCC_OscInitStruct.PLL.PLLFRACN   = 0;
+    RCC_OscInitStruct.OscillatorType =
+        RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState      = RCC_HSE_ON;
+    RCC_OscInitStruct.LSIState      = RCC_LSI_ON;
+    RCC_OscInitStruct.PLL.PLLState  = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM      = 5;
+    RCC_OscInitStruct.PLL.PLLN      = 192;
+    RCC_OscInitStruct.PLL.PLLP      = 2;
+    RCC_OscInitStruct.PLL.PLLQ      = 4;
+    RCC_OscInitStruct.PLL.PLLR      = 2;
+    RCC_OscInitStruct.PLL.PLLRGE    = RCC_PLL1VCIRANGE_2;
+    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+    RCC_OscInitStruct.PLL.PLLFRACN  = 0;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) { Error_Handler(); }
 
     /** Initializes the CPU, AHB and APB buses clocks
      */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 |
-                                  RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 |
+                                  RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
     RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.SYSCLKDivider  = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.AHBCLKDivider  = RCC_HCLK_DIV2;
@@ -204,7 +196,9 @@ void SystemClock_Config(void)
     RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
     RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) { Error_Handler(); }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
