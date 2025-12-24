9a3b70c6d004ba4dae9dea4773baf5e8e0309b1e
JontyLi <ljt20030312@Outlook.com>
2025-12-24 20:31:11 +0800

    🌈 style(.ld, map.h): change section name

--- App/Inc/map.h
+++ App/Inc/map.h
@@ -3,12 +3,11 @@
 
 #define PLACE_IN_BUFFER_SECTION __attribute__((section(".buffer")))
 
-#define PLACE_IN_RAM_SECTION __attribute__((section(".ram")))
+#define PLACE_IN_RAM_SECTION    __attribute__((section(".ram_section")))
 
 #define PLACE_IN_QSPI_SECTION                                                  \
     __attribute__((section(".qspi_section"))) __attribute__((used))
 
 #define PLACE_IN_SDRAM_SECTION                                                 \
-    __attribute__((section(".sdram_buffer"))) __attribute__((aligned(32)))
-
+    __attribute__((section(".sdram_section"))) __attribute__((aligned(32)))
 #endif
