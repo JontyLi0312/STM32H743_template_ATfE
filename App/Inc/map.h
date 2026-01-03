a49fb71da6acc6bce78f300d6d5d74779a9d19b7
JontyLi <ljt20030312@Outlook.com>
2026-01-03 15:00:24 +0800

    🐞 fix(App/Inc/map.h): remove useless macro

--- App/Inc/map.h
+++ App/Inc/map.h
@@ -1,8 +1,6 @@
 #ifndef MAP_H_
 #define MAP_H_
 
-#define PLACE_IN_BUFFER_SECTION __attribute__((section(".buffer")))
-
 #define PLACE_IN_RAM_SECTION    __attribute__((section(".ram_section")))
 
 #define PLACE_IN_QSPI_SECTION                                                  \
