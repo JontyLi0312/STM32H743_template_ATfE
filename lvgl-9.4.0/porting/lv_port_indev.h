ad3fd247f0de9c5d7749858b583bde65da8d8240
JontyLi <ljt20030312@Outlook.com>
2026-01-03 17:29:27 +0800

    🐞 fix(lvgl-9.4.0/porting/lv_port_disp.h): modify header file path

--- lvgl-9.4.0/porting/lv_port_indev.h
+++ lvgl-9.4.0/porting/lv_port_indev.h
@@ -4,7 +4,8 @@
  *
  */
 
-/*Copy this file as "lv_port_indev.h" and set this value to "1" to enable content*/
+/*Copy this file as "lv_port_indev.h" and set this value to "1" to enable
+ * content*/
 #if 1
 
 #ifndef LV_PORT_INDEV_TEMPL_H
@@ -20,7 +21,7 @@ extern "C" {
 #if defined(LV_LVGL_H_INCLUDE_SIMPLE)
 #include "lvgl.h"
 #else
-#include "lvgl/lvgl.h"
+#include "lvgl.h"
 #endif
 
 /*********************
