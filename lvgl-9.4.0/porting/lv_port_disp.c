f2e903f6e075cfdedc7ea3bae61e418cfc7fb7a2
JontyLi <ljt20030312@Outlook.com>
2026-01-03 14:43:00 +0800

    ✨ feat(lvgl-9.4.0/porting/lv_port_disp.c): port display

--- lvgl-9.4.0/porting/lv_port_disp.c
+++ lvgl-9.4.0/porting/lv_port_disp.c
@@ -3,7 +3,8 @@
  *
  */
 
-/*Copy this file as "lv_port_disp.c" and set this value to "1" to enable content*/
+/*Copy this file as "lv_port_disp.c" and set this value to "1" to enable
+ * content*/
 #if 1
 
 /*********************
@@ -12,20 +13,29 @@
 #include "lv_port_disp.h"
 #include <stdbool.h>
 
+#include "ltdc.h"
+
 /*********************
  *      DEFINES
  *********************/
+#define MY_DISP_HOR_RES 800
+#define MY_DISP_VER_RES 480
+
+extern LTDC_HandleTypeDef hltdc;
+
 #ifndef MY_DISP_HOR_RES
-    #warning Please define or replace the macro MY_DISP_HOR_RES with the actual screen width, default value 320 is used for now.
-    #define MY_DISP_HOR_RES    320
+#warning Please define or replace the macro MY_DISP_HOR_RES with the actual screen width, default value 320 is used for now.
+#define MY_DISP_HOR_RES 320
 #endif
 
 #ifndef MY_DISP_VER_RES
-    #warning Please define or replace the macro MY_DISP_VER_RES with the actual screen height, default value 240 is used for now.
-    #define MY_DISP_VER_RES    240
+#warning Please define or replace the macro MY_DISP_VER_RES with the actual screen height, default value 240 is used for now.
+#define MY_DISP_VER_RES 240
 #endif
 
-#define BYTE_PER_PIXEL (LV_COLOR_FORMAT_GET_SIZE(LV_COLOR_FORMAT_RGB565)) /*will be 2 for RGB565 */
+#define BYTE_PER_PIXEL                                                         \
+    (LV_COLOR_FORMAT_GET_SIZE(LV_COLOR_FORMAT_RGB565)) /*will be 2 for RGB565  \
+                                                        */
 
 /**********************
  *      TYPEDEFS
@@ -36,7 +46,8 @@
  **********************/
 static void disp_init(void);
 
-static void disp_flush(lv_display_t * disp, const lv_area_t * area, uint8_t * px_map);
+static void disp_flush(lv_display_t *disp, const lv_area_t *area,
+                       uint8_t *px_map);
 
 /**********************
  *  STATIC VARIABLES
@@ -60,35 +71,42 @@ void lv_port_disp_init(void)
     /*------------------------------------
      * Create a display and set a flush_cb
      * -----------------------------------*/
-    lv_display_t * disp = lv_display_create(MY_DISP_HOR_RES, MY_DISP_VER_RES);
+    lv_display_t *disp = lv_display_create(MY_DISP_HOR_RES, MY_DISP_VER_RES);
     lv_display_set_flush_cb(disp, disp_flush);
 
-    /* Example 1
-     * One buffer for partial rendering*/
-    LV_ATTRIBUTE_MEM_ALIGN
-    static uint8_t buf_1_1[MY_DISP_HOR_RES * 10 * BYTE_PER_PIXEL];            /*A buffer for 10 rows*/
-    lv_display_set_buffers(disp, buf_1_1, NULL, sizeof(buf_1_1), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    // /* Example 1
+    //  * One buffer for partial rendering*/
+    // LV_ATTRIBUTE_MEM_ALIGN
+    // static uint8_t
+    //     buf_1_1[MY_DISP_HOR_RES * 10 * BYTE_PER_PIXEL]; /*A buffer for 10
+    //     rows*/
+    // lv_display_set_buffers(disp, buf_1_1, NULL, sizeof(buf_1_1),
+    //                        LV_DISPLAY_RENDER_MODE_PARTIAL);
 
-    /* Example 2
-     * Two buffers for partial rendering
-     * In flush_cb DMA or similar hardware should be used to update the display in the background.*/
-    LV_ATTRIBUTE_MEM_ALIGN
-    static uint8_t buf_2_1[MY_DISP_HOR_RES * 10 * BYTE_PER_PIXEL];
+    // /* Example 2
+    //  * Two buffers for partial rendering
+    //  * In flush_cb DMA or similar hardware should be used to update the
+    //  display
+    //  * in the background.*/
+    // LV_ATTRIBUTE_MEM_ALIGN
+    // static uint8_t buf_2_1[MY_DISP_HOR_RES * 10 * BYTE_PER_PIXEL];
 
-    LV_ATTRIBUTE_MEM_ALIGN
-    static uint8_t buf_2_2[MY_DISP_HOR_RES * 10 * BYTE_PER_PIXEL];
-    lv_display_set_buffers(disp, buf_2_1, buf_2_2, sizeof(buf_2_1), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    // LV_ATTRIBUTE_MEM_ALIGN
+    // static uint8_t buf_2_2[MY_DISP_HOR_RES * 10 * BYTE_PER_PIXEL];
+    // lv_display_set_buffers(disp, buf_2_1, buf_2_2, sizeof(buf_2_1),
+    //                        LV_DISPLAY_RENDER_MODE_PARTIAL);
 
     /* Example 3
      * Two buffers screen sized buffer for double buffering.
-     * Both LV_DISPLAY_RENDER_MODE_DIRECT and LV_DISPLAY_RENDER_MODE_FULL works, see their comments*/
+     * Both LV_DISPLAY_RENDER_MODE_DIRECT and LV_DISPLAY_RENDER_MODE_FULL works,
+     * see their comments*/
     LV_ATTRIBUTE_MEM_ALIGN
     static uint8_t buf_3_1[MY_DISP_HOR_RES * MY_DISP_VER_RES * BYTE_PER_PIXEL];
 
     LV_ATTRIBUTE_MEM_ALIGN
     static uint8_t buf_3_2[MY_DISP_HOR_RES * MY_DISP_VER_RES * BYTE_PER_PIXEL];
-    lv_display_set_buffers(disp, buf_3_1, buf_3_2, sizeof(buf_3_1), LV_DISPLAY_RENDER_MODE_DIRECT);
-
+    lv_display_set_buffers(disp, buf_3_1, buf_3_2, sizeof(buf_3_1),
+                           LV_DISPLAY_RENDER_MODE_DIRECT);
 }
 
 /**********************
@@ -103,14 +121,16 @@ static void disp_init(void)
 
 volatile bool disp_flush_enabled = true;
 
-/* Enable updating the screen (the flushing process) when disp_flush() is called by LVGL
+/* Enable updating the screen (the flushing process) when disp_flush() is called
+ * by LVGL
  */
 void disp_enable_update(void)
 {
     disp_flush_enabled = true;
 }
 
-/* Disable updating the screen (the flushing process) when disp_flush() is called by LVGL
+/* Disable updating the screen (the flushing process) when disp_flush() is
+ * called by LVGL
  */
 void disp_disable_update(void)
 {
@@ -118,28 +138,24 @@ void disp_disable_update(void)
 }
 
 /*Flush the content of the internal buffer the specific area on the display.
- *`px_map` contains the rendered image as raw pixel map and it should be copied to `area` on the display.
- *You can use DMA or any hardware acceleration to do this operation in the background but
- *'lv_display_flush_ready()' has to be called when it's finished.*/
-static void disp_flush(lv_display_t * disp_drv, const lv_area_t * area, uint8_t * px_map)
+ *`px_map` contains the rendered image as raw pixel map and it should be copied
+ * to `area` on the display. You can use DMA or any hardware acceleration to do
+ * this operation in the background but 'lv_display_flush_ready()' has to be
+ * called when it's finished.*/
+static void disp_flush(lv_display_t *disp_drv, const lv_area_t *area,
+                       uint8_t *px_map)
 {
-    if(disp_flush_enabled) {
-        /*The most simple case (but also the slowest) to put all pixels to the screen one-by-one*/
-
-        int32_t x;
-        int32_t y;
-        for(y = area->y1; y <= area->y2; y++) {
-            for(x = area->x1; x <= area->x2; x++) {
-                /*Put a pixel to the display. For example:*/
-                /*put_px(x, y, *px_map)*/
-                px_map++;
-            }
-        }
-    }
+    if (disp_flush_enabled) { LTDC_Layer1->CFBAR = (uint32_t)px_map; }
 
     /*IMPORTANT!!!
      *Inform the graphics library that you are ready with the flushing*/
     lv_display_flush_ready(disp_drv);
+}
+
+void HAL_LTDC_LineEvenCallback(LTDC_HandleTypeDef *hltdc)
+{
+    __HAL_LTDC_RELOAD_CONFIG(hltdc);
+    HAL_LTDC_ProgramLineEvent(hltdc, 0);
 }
 
 #else /*Enable this file at the top*/
