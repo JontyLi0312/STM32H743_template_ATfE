2421d5a65c16c31d5424776ede90308aa2416441
JontyLi <ljt20030312@Outlook.com>
2026-01-03 17:26:21 +0800

    🔧 build(cmake/ATfE.cmake): change mcu specific flags

--- cmake/ATfE.cmake
+++ cmake/ATfE.cmake
@@ -21,16 +21,16 @@ set(CMAKE_EXECUTABLE_SUFFIX_CXX 	".elf")
 set(CMAKE_TRY_COMPILE_TARGET_TYPE 	STATIC_LIBRARY)
 
 # MCU specific flags
-set(TARGET_FLAGS 	"--target=armv7m-none-eabi  -mfpu=fpv5-d16 -mfloat-abi=hard")
+set(TARGET_FLAGS 	"--target=arm-none-eabi -mcpu=cortex-m7  -mfpu=fpv5-d16 -mfloat-abi=hard")
 
 set(CMAKE_C_FLAGS 	"${CMAKE_C_FLAGS} ${TARGET_FLAGS}")
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MP")
 set(CMAKE_C_FLAGS	"${CMAKE_C_FLAGS} -Wall -fdata-sections -ffunction-sections")
 
 set(CMAKE_C_FLAGS_DEBUG 	"-O0 -g3")
-set(CMAKE_C_FLAGS_RELEASE 	"-Os -g0")
+set(CMAKE_C_FLAGS_RELEASE 	"-O3 -g0")
 set(CMAKE_CXX_FLAGS_DEBUG 	"-O0 -g3")
-set(CMAKE_CXX_FLAGS_RELEASE "-Os -g0")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -g0")
 
 set(CMAKE_CXX_FLAGS			"${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
