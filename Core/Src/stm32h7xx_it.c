8f75bfa9ec333a5fa579998b5977645279cb2906
JontyLi <ljt20030312@Outlook.com>
2026-01-08 14:15:32 +0800

    🐞 fix(Core\Src): 修改 lvgl 心跳代码位置，避免 cubemx 将其覆写

--- Core/Src/stm32h7xx_it.c
+++ Core/Src/stm32h7xx_it.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "lvgl.h"
+#include "src/hal/lv_hal_tick.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -185,8 +186,8 @@ void SysTick_Handler(void)
 
     /* USER CODE END SysTick_IRQn 0 */
     HAL_IncTick();
-    lv_tick_inc(1);
     /* USER CODE BEGIN SysTick_IRQn 1 */
+    lv_tick_inc(1);
     /* USER CODE END SysTick_IRQn 1 */
 }
 
