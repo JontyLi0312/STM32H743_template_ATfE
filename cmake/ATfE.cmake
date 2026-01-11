9ac8b1954346fe9c29926a402dc2fa5ef55955df
JontyLi <ljt20030312@Outlook.com>
2026-01-12 00:47:20 +0800

    🌈 style(ATfE.cmake): remove useless space

--- cmake/ATfE.cmake
+++ cmake/ATfE.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME 		Generic)
 set(CMAKE_SYSTEM_PROCESSOR 	arm)
 
-set(CMAKE_C_COMPILER_ID 		Clang)
-set(CMAKE_CXX_COMPILER_ID 		Clang)
+set(CMAKE_C_COMPILER_ID 	Clang)
+set(CMAKE_CXX_COMPILER_ID 	Clang)
 
 # ATfE path must be set in here.
 set(TOOLCHAIN_PREFIX	"D:/Toolchain/ATfE-21.1.1/bin/")
@@ -21,7 +21,7 @@ set(CMAKE_EXECUTABLE_SUFFIX_CXX 	".elf")
 set(CMAKE_TRY_COMPILE_TARGET_TYPE 	STATIC_LIBRARY)
 
 # MCU specific flags
-set(TARGET_FLAGS 	"--target=arm-none-eabi -mcpu=cortex-m7  -mfpu=fpv5-d16 -mfloat-abi=hard")
+set(TARGET_FLAGS 	"--target=arm-none-eabi -mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard")
 
 set(CMAKE_C_FLAGS 	"${CMAKE_C_FLAGS} ${TARGET_FLAGS}")
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MP")
@@ -32,7 +32,7 @@ set(CMAKE_C_FLAGS_RELEASE 	"-O3 -g3")
 set(CMAKE_CXX_FLAGS_DEBUG 	"-O0 -g3")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -g3")
 
-set(CMAKE_CXX_FLAGS			"${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(CMAKE_CXX_FLAGS	"${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
 set(CMAKE_CXX_FLAGS "${TARGET_FLAGS}")
 
