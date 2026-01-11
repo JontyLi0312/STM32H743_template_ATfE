a6e414563f06740bf31eac3f76dd6f503dcb7804
JontyLi <ljt20030312@Outlook.com>
2026-01-11 14:51:19 +0800

    ✨ feat(App\Inc\map.h): adaptation to .ld modification

--- App/Inc/map.h
+++ App/Inc/map.h
@@ -1,11 +1,12 @@
-#ifndef MAP_H_
-#define MAP_H_
+#ifndef __MAP_H
+#define __MAP_H
 
-#define PLACE_IN_RAM_SECTION    __attribute__((section(".ram_section")))
-
-#define PLACE_IN_QSPI_SECTION                                                  \
-    __attribute__((section(".qspi_section"))) __attribute__((used))
-
+#define PLACE_IN_RAM_SECTION __attribute__((section(".ram_section")))
+#define PLACE_IN_LVGL_FB                                                       \
+    __attribute__((section(".lvgl_fb"))) __attribute__((aligned(32)))
 #define PLACE_IN_SDRAM_SECTION                                                 \
     __attribute__((section(".sdram_section"))) __attribute__((aligned(32)))
+#define PLACE_IN_QSPI_SECTION __attribute__((section(".qspi_sectoin")))
+#define PLACE_IN_LVGL_ASSETS  __attribute__((section(".lvgl_assets")))
+
 #endif
